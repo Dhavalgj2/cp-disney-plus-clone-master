d99abd169e44de28b461086377436572e58e24ef
Dhaval <71810466+Dhavalgj2@users.noreply.github.com>
2022-08-29 07:01:05 +1000

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,32 @@
 import styled from "styled-components";
 import React from "react";
 import { useSelector } from "react-redux";
+import ImgSlider from "./ImgSlider";
 
 const Home = () => {
   const user = useSelector((state) => state.user.name);
-  return <Container>{user}</Container>;
+  return (
+    <Container>
+      <ImgSlider />
+    </Container>
+  );
 };
 const Container = styled.main`
   position: relative;
-  background: url("/images/home-background.png");
+  min-height: calc(100vh - 250px)
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &: after{
+    background: url("./images/home-background.png") center center / cover
+    no-repeat fixed;    
+    content: "";
+    position: absolute;
+   inset: 0px;
+   opacity: 1;
+   z-index: -1;
+  }
 `;
 export default Home;
